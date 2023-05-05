b7595958c639b203d1504f0411dd093ced021640
heliosalesjr <heliosalesjr@gmail.com>
2023-05-05 18:13:26 -0300

    useFetch updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
+import RecipeList from '../../components/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
+
 import './Home.css';
 
 import React from 'react'
 
 function Home() {
+
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+
+
   return (
-    <div>Home</div>
+
+    <div className='home'>
+
+      { error && <p className='error'>{ error } </p> }
+      { isPending && <p className='loading'>Loading...</p> }
+      { data && <RecipeList recipes={data}/> }
+
+
+    </div>
   )
 }
 
